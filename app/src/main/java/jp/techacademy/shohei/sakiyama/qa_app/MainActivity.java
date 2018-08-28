2dc10bde14644a4c30d6b6ab9e9dd8175fb90786
sakiyama-s <sakiyama.s-hu@nhk.or.jp>
2018-08-28 13:11:00 +0900

    課題前まで

--- app/src/main/java/jp/techacademy/shohei/sakiyama/qa_app/MainActivity.java
+++ app/src/main/java/jp/techacademy/shohei/sakiyama/qa_app/MainActivity.java
@@ -16,6 +16,7 @@ import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -160,7 +161,6 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        // --- ここから ---
         // Firebase
         mDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
@@ -169,7 +169,16 @@ public class MainActivity extends AppCompatActivity
         mAdapter = new QuestionsListAdapter(this);
         mQuestionArrayList = new ArrayList<Question>();
         mAdapter.notifyDataSetChanged();
-        // --- ここまで追加する ---
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Questionのインスタンスを渡して質問詳細画面を起動する
+                Intent intent = new Intent(getApplicationContext(), QuestionDetailActivity.class);
+                intent.putExtra("question", mQuestionArrayList.get(position));
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
