8194dfeea8ead378dc48abd7e3024b2ba9913072
sakiyama-s <sakiyama.s-hu@nhk.or.jp>
2018-09-03 19:34:41 +0900

    お気に入りボタンとfirebaseの連携(状態表示までver0903）

--- app/src/main/java/jp/techacademy/shohei/sakiyama/qa_app/Favorite.java
+++ app/src/main/java/jp/techacademy/shohei/sakiyama/qa_app/Favorite.java
@@ -3,21 +3,27 @@ package jp.techacademy.shohei.sakiyama.qa_app;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Favorite implements Serializable{
+public class Favorite implements Serializable {
     private String mFavId;
     private String mQuestionUid;
+    private String mGenre;
     private ArrayList<String> mQuestionUidList;
 
-    public Favorite(String favId, String questionUid){
+    public Favorite(String favId, String questionUid, String genre) {
         mFavId = favId;
         mQuestionUid = questionUid;
+        mGenre = genre;
     }
 
-    public String getFavId(){
+    public String getFavId() {
         return mFavId;
     }
 
-    public String getQuestionUid(){
+    public String getQuestionUid() {
         return mQuestionUid;
+    }
+
+    public String getGenre() {
+        return mGenre;
     }
 }
--- app/src/main/java/jp/techacademy/shohei/sakiyama/qa_app/FavoriteListActivity.java
+++ app/src/main/java/jp/techacademy/shohei/sakiyama/qa_app/FavoriteListActivity.java
@@ -1,13 +1,90 @@
 package jp.techacademy.shohei.sakiyama.qa_app;
 
+import android.content.Intent;
+import android.support.constraint.solver.widgets.Snapshot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FavoriteListActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private int mGenre = 0;
+
+    private DatabaseReference mDatabaseReference;
+    private DatabaseReference mGenreRef;
+    private ListView mListView;
+    private ArrayList<Question> mQuestionArrayList;
+    private ArrayList<Favorite> mFavoriteArrayList;
+    private QuestionsListAdapter mAdapter;
+
+    private ChildEventListener mFavoriteEventListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+//            mDatabaseReference =
+//            HashMap map= (HashMap) dataSnapshot.getValue();
+//            String genre = (String) map.get("genre");
+
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_list);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+        // ListViewの準備
+        mListView = (ListView) findViewById(R.id.listView);
+        mAdapter = new QuestionsListAdapter(this);
+        mQuestionArrayList = new ArrayList<Question>();
+        mAdapter.notifyDataSetChanged();
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Questionのインスタンスを渡して質問詳細画面を起動する
+                Intent intent = new Intent(getApplicationContext(), QuestionDetailActivity.class);
+                intent.putExtra("question", mQuestionArrayList.get(position));
+                startActivity(intent);
+            }
+        });
     }
 }
